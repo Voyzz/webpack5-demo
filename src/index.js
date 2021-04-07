9d5a8ec69672b8d179464ce7b6dbf8523bb16e5b
Voyz <18217501371@163.com>
2021-04-07 17:21:33 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-// import Babel from './script/Babel';
-// import Fonts from './script/Fonts';
 // import HMR from './script/HMR'
 // import Header from './script/Header';
 import "@babel/polyfill";
 
 import React,{Component} from 'react';
 
+// import Babel from './script/Babel';
+import Fonts from './script/Fonts';
 import ReactDom from 'react-dom';
 
 class App extends Component{
     render (){
         return (
-            <div>hello world</div>
+            <Fonts/>
         )
     }
 }
